68f3a7287a86ab479216fc4c1d7d1cc08f89b74d
Varya <varya.zaytseva@icloud.com>
2023-05-29 13:30:14 +0300

    изменили

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,54 +1,54 @@
-<nav>
-    <div class="nav-container">
-      <a href="/" class="nav-logo" title="Back to Homepage">Варя Зайцева</a>
-      <div class="nav-links">
-        {#if link.isShow}
-<a href={link.href} class="link">{link.title}</a>
-{/if}
-
-
-      </div>
-    </div>  
-  </nav>
-  
-  <div class="container">
-    <!-- Pages will be injected below -->
-    <slot></slot>
-  </div>
-  <script>
-    const nav = [
+<script>
+  const nav = [
     {
-      title: 'Обо мне',
-      href: '/about',
-      isShow: true
+      title: "Обо мне",
+      href: "/about",
+      isShow: true,
     },
     {
-      title: 'Блог',
-      href: '/blog',
-      isShow: true
+      title: "Блог",
+      href: "/blog",
+      isShow: true,
     },
     {
-      title: 'Портфолио',
-      href: '/project',
-      isShow: true
+      title: "Портфолио",
+      href: "/project",
+      isShow: true,
     },
     {
-      title: 'Контакты',
-      href: '/contact',
-      isShow: true
+      title: "Контакты",
+      href: "/contact",
+      isShow: true,
     },
     {
       title: "temp",
       href: "/temp",
-      isShow: false
-    }
-  ]
+      isShow: false,
+    },
+  ];
 </script>
 
-  <style>
-    .container {
-      max-width: 1400px;
-      margin: 50px auto;
-    }
-  </style>
-  
+<nav>
+  <div class="nav-container">
+    <a href="/" class="nav-logo" title="Back to Homepage">Варя Зайцева</a>
+    <div class="nav-links">
+      {#each nav as link}
+        {#if link.isShow}
+          <a href={link.href} class="link">{link.title}</a>
+        {/if}
+      {/each}
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+  <!-- Pages will be injected below -->
+  <slot />
+</div>
+
+<style>
+  .container {
+    max-width: 1400px;
+    margin: 50px auto;
+  }
+</style>
