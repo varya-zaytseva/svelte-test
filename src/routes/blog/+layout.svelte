18abeb414eafb4fdc000a93f7b24ff95a141c518
Varya <varya.zaytseva@icloud.com>
2023-05-29 14:16:56 +0300

    site

--- src/routes/blog/+layout.svelte
+++ src/routes/blog/+layout.svelte
@@ -1,4 +1,27 @@
 <div class="container">
     <slot />
-  </div>
-  
+</div>
+
+<style>
+    .container {
+        margin: 50px auto;
+        max-width: 800px;
+        padding: 0 20px;
+    }
+
+    .blogposts {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        grid-gap: 20px;
+    }
+
+    .post {
+        border: 1px solid #ddd;
+        padding: 10px;
+        box-shadow: 0 0 10px #eee;
+    }
+
+    .link {
+        color: rgb(10, 10, 139);
+    }
+</style>
