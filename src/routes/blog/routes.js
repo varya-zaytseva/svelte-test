341495032e5e6a33a1ed7802a43e8a4ac2349f44
Varya <varya.zaytseva@icloud.com>
2023-05-29 15:05:35 +0300

    jkhsd

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -4,5 +4,11 @@ export const routes = [
       title: 'My First Blog Post',
       body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
     },
+    {
+        id: 2,
+        title: "My Second Blog Post",
+        body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate"
+    }
+      
   ];
   
