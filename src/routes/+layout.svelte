0ca71c85f7b89a5de8904ca062489b45d53a2179
Varya <varya.zaytseva@icloud.com>
2023-05-29 13:24:31 +0300

    внесли изменения

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,10 @@
     <div class="nav-container">
       <a href="/" class="nav-logo" title="Back to Homepage">Варя Зайцева</a>
       <div class="nav-links">
-        {#each nav as link}
+        {#if link.isShow}
 <a href={link.href} class="link">{link.title}</a>
-{/each}
+{/if}
+
 
       </div>
     </div>  
@@ -18,23 +19,28 @@
     const nav = [
     {
       title: 'Обо мне',
-      href: '/about'
+      href: '/about',
+      isShow: true
     },
     {
       title: 'Блог',
-      href: '/blog'
+      href: '/blog',
+      isShow: true
     },
     {
       title: 'Портфолио',
-      href: '/project'
+      href: '/project',
+      isShow: true
     },
     {
       title: 'Контакты',
-      href: '/contact'
+      href: '/contact',
+      isShow: true
     },
     {
       title: "temp",
-      href: "/temp"
+      href: "/temp",
+      isShow: false
     }
   ]
 </script>
