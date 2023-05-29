55715c2e3d7c0c6c3fff4caf1d39b215d390978f
Varya <varya.zaytseva@icloud.com>
2023-05-29 14:06:07 +0300

    site

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -13,30 +13,30 @@
                         Читать далее
                     </a>
                 </p>
-                <style>
-                    .container {
-                        margin: 50px auto;
-                        max-width: 800px;
-                        padding: 0 20px;
-                    }
-
-                    .blogposts {
-                        display: grid;
-                        grid-template-columns: 1fr 1fr;
-                        grid-gap: 20px;
-                    }
-
-                    .post {
-                        border: 1px solid #ddd;
-                        padding: 10px;
-                        box-shadow: 0 0 10px #eee;
-                    }
-
-                    .link {
-                        color: rgb(10, 10, 139);
-                    }
-                </style>
             </div>
         {/each}
     </div>
+    <style>
+        .container {
+          margin: 50px auto;
+          max-width: 800px;
+          padding: 0 20px;
+        }
+      
+        .blogposts {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          grid-gap: 20px;
+        }
+      
+        .post {
+          border: 1px solid #ddd;
+          padding: 10px;
+          box-shadow: 0 0 10px #eee;
+        }
+      
+        .link {
+          color: rgb(10, 10, 139);
+        }
+      </style>
 </div>
